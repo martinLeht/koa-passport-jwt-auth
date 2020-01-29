41cf65af02acfde54e35af01747f501676e5352c
Klaus <klaus.jokinen@edu.turkuamk.fi>
2020-01-29 11:31:32 +0200

    Return error message in response

--- auth-server/src/app.ts
+++ auth-server/src/app.ts
@@ -34,9 +34,8 @@ export class App {
             } catch (err) {
                 ctx.status = err.status || 500;
                 ctx.body = { 
-                    error: parseInt(err.message) 
+                    error: err.message
                 };
-//                ctx.app.emit('error', err, ctx);
             }
         });
 
@@ -48,7 +47,7 @@ export class App {
 
     public async start() {
         const app = await this.createApp();
-        var port = process.env.PORT || 3000;
+        const port = process.env.PORT || 3000;
         const server = app.listen(port);
         console.log('Server started listening on port: ' + port);
         return Promise.resolve(server);
