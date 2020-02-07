233710e33229f0caef2acab46d61aa39b16ef398
sakariwahlsten <32675163+sakariwahlsten@users.noreply.github.com>
2020-02-07 21:16:44 +0200

    Update app.module.ts
    Removed LoginComponent

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserInformationComponent } from './userinformation/userinformation.component';
 import { ProfileComponent } from './profile/profile.component';
-import { LoginRoutingModule } from './login/login-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     RegisterComponent,
     UserInformationComponent,
     ProfileComponent
