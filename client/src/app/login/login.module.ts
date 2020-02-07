e8ff66d75eb732dbca77f1e4847db9870dfc84af
sakariwahlsten <32675163+sakariwahlsten@users.noreply.github.com>
2020-02-08 00:39:41 +0200

    Login made with angular materials
    Added login card and form with email and password fields. Login and register buttons + routing to register page.

--- client/src/app/login/login.module.ts
+++ client/src/app/login/login.module.ts
@@ -1,17 +1,31 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent} from './login.component';
 
-import {MatMenuModule} from '@angular/material/menu';
+//Materials
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule
+} from '@angular/material/';
+import {ReactiveFormsModule, FormControl, Validators} from '@angular/forms';
+
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
-    MatMenuModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule
   ]
 })
 export class LoginModule { }
