476eb2135c6e16b8e7766801046e7d5aa40c3d6a
sakariwahlsten <32675163+sakariwahlsten@users.noreply.github.com>
2020-02-07 10:25:21 +0200

    client front
    initial

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserInformationComponent } from './userinformation/userinformation.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserInformationComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/userinformation/userinformation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userinformation',
+  templateUrl: './userinformation.component.html',
+  styleUrls: ['./userinformation.component.scss']
+})
+export class UserInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
