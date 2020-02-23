5dd42888decdce2e885a8e27af40c6d331c17d68
sakariwahlsten <32675163+sakariwahlsten@users.noreply.github.com>
2020-02-23 20:38:13 +0200

    Login form validation/Http post
    Added form validation for login, it's not perfect but it works. Regexp for password could be modified to allow special chars.
    Added Http modules and some kind of post function but not sure if it even works, I'll try it later and try to figure how to use the response.

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { LoginPost } from './loginPost';
+//import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +11,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  // Form state
+  loading = false;
+  success = false;
+
+  // HTTP root
+  readonly ROOT_URL = 'http://localhost:3000';
+
+  loginPost: any;
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   ngOnInit() {
+    // Init login form
+    this.loginForm = this.fb.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]
+    ],
+      password: ['',[
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(32),
+        Validators.pattern('^(?=.*[0-9])(?=[a-zA-Z])([a-zA-Z0-9]+)$')//([ !#,-_&\$\.\?+]) special chars
+      ]
+    ],
+    })
+  }
+  // Get functions
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  // RxJS submit
+  async submitHandler() {
+    this.loading = true;
+    /*const emailValue = this.loginForm.get('email').value;
+    const passwordValue = this.loginForm.get('password').value;
+    console.log(emailValue);
+    console.log(passwordValue);
+
+    // Post data
+    const data: LoginPost = {
+      email: emailValue,
+      password: passwordValue
+    }
+    */
+
+    // Try to send the data
+    try {
+      this.loginPost = this.http.post(this.ROOT_URL + '/login', this.loginForm.value);
+      this.success = true;
+    } catch(err) {
+      console.error(err);
+    }
+    
+    this.loading = false;
   }
 
 }
