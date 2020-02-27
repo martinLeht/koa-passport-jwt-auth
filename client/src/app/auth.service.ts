233f22910d71bac38495e4e74bbedfc0561a5361
sakariwahlsten <32675163+sakariwahlsten@users.noreply.github.com>
2020-02-28 01:43:27 +0200

    Auth service implemented on login component
    HTTP post and login function in auth service. Now implemented in login component. Should figure out how to use the subscription so we can show user success in case of a successfull login and error if login failed. At the moment the boolean that is printed comes before the login event has even happend. I believe it's because the login happens asynchronously.
    
    There is a bug in the password hiding button at the moment, once hiding button is clicked page tries to log user in. No idea why...

--- client/src/app/auth.service.ts
+++ client/src/app/auth.service.ts
@@ -1,20 +1,58 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
+interface LoginResp{
+  token: string
+}
+interface UserObj{
+  id: number,
+  username: string,
+  email: string,
+  activationToken: string,
+  active: number
+}
+interface RegisterResp{
+  success: string,
+  user: object,
+}
+var loginSuccess = false;
+
 @Injectable()
 export class AuthService {
-
   constructor(private http: HttpClient) { }
 
   // HTTP root
   readonly ROOT_URL = 'http://localhost:3000';
-  authToken;
+  authToken: LoginResp;
 
-  loginUser(post: any){
-    try{
-      return this.http.post(this.ROOT_URL + '/login', post);
-    }catch(err){
-      console.log(err);
-    }
+  loginUser(post: any): boolean{
+    this.http.post<LoginResp>(this.ROOT_URL + '/login', post).subscribe(data =>{
+      this.authToken = data;
+      console.log(this.authToken.token);
+       loginSuccess = true;
+    },
+    error => {
+      console.log(error);
+      loginSuccess = false;
+    });
+    return loginSuccess;
   }
+
+  setUserDetails(){
+
+  }
+
+  getUserDetails(){
+
+  }
+
+  logOutUser(){
+
+  }
+
+  setCookie(name: string, value: string, exp: number, ){
+
+  }
+
 }
