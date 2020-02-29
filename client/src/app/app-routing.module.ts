f44f1cd94744928b5fed5dab7b52d78c18f7e83e
N6397 <N6397@student.jamk.fi>
2020-02-29 11:33:33 +0200

    Made folders for angular Components and Services.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {ProfileComponent}   from './profile/profile.component';
+import {ProfileComponent}   from './components/profile/profile.component';
 
 
 const routes: Routes = [
-  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
-  {path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule)},
-  {path: 'userinformation', loadChildren: () => import('./userinformation/userinformation.module').then(m => m.UserinformationModule)},
+  {path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)},
+  {path: 'register', loadChildren: () => import('./components/register/register.module').then(m => m.RegisterModule)},
+  {path: 'userinformation', loadChildren: () => import('./components/userinformation/userinformation.module').then(m => m.UserinformationModule)},
   {path: 'profile', component: ProfileComponent},
   {path: '', redirectTo: '', pathMatch: 'full'}
 ];
