baeeaaf2ba0a7164c0ddcdc17c83376ffc00faa5
Martin Lehtomaa <martin.lehtomaa@gmail.com>
2020-04-05 14:01:51 +0300

    Cleaned away commented block

--- client/src/app/helpers/auth.interceptor.ts
+++ client/src/app/helpers/auth.interceptor.ts
@@ -12,18 +12,6 @@ const TOKEN_HEADER_KEY = 'Authorization';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  /*
-  constructor(private tokenStorage: TokenStorageService) { }
-
-  intercept(req: HttpRequest<any>, next: HttpHandler) {
-    let authReq = req;
-    const token = this.tokenStorage.getToken();
-    if (token != null) {
-      authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, 'Bearer ' + token) });
-    }
-    return next.handle(authReq);
-  }
-  */
 
   private refreshingInProgress: boolean;
   private accessTokenSubject: BehaviorSubject<string> = new BehaviorSubject<string>(null);
